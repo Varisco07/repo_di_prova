69e30c939381e2cdd6c8a9ac9a01264e41aee326
NICOLO' VARISCO <varisco.nicolo.07@itisdalmine.edu.it>
2024-12-17 10:01:15 +0100

    Implementazione del menu

--- calcolatrice.py
+++ calcolatrice.py
@@ -2,23 +2,9 @@ def somma():
     n_1 = int(input("Inserisci il primo numero: "))
     n_2 = int(input("Inserisci il secondo numero: "))
     print(f"{n_1} + {n_2} = {n_1 + n_2}")
-    
+
 def sottrazione():
     n_1 = int(input("Inserisci il primo numero: "))
     n_2 = int(input("Inserisci il secondo numero: "))
     print(f"{n_1} - {n_2} = {n_1 - n_2}")
-
-
-while True:
-    print("Benveuto nella nostra calcolatrice")
-    print("Inserisci l'operazione che vuoi effettuare")
-    scelta = int(input("1)sottrazione\n2)addizione\n0)exit"))
-    if scelta == 0:
-        break
-    elif scelta == 1:
-        sottrazione()
-    elif scelta == 2:
-        somma()
-    else: 
-        print("Scelta non corretta!!")
-
+    
