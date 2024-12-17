9086cc140de1e25250ee539b89283fd754baa9bc
NICOLO' VARISCO <varisco.nicolo.07@itisdalmine.edu.it>
2024-12-17 09:39:29 +0100

    Fixed big with the 0

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,10 +1,10 @@
-scelta = -1
-while scelta != 0:
+while True:
     print("Benveuto nella nostra calcolatrice")
     print("Inserisci l'operazione che vuoi effettuare")
     scelta = int(input("1)sottrazione\n2)addizione\n0)exit"))
-    
-    if scelta == 1:
+    if scelta == 0:
+        break
+    elif scelta == 1:
         n_1 = int(input("Inserisci il primo numero: "))
         n_2 = int(input("Inserisci il secondo numero: "))
         print(f"{n_1} - {n_2} = {n_1 - n_2}")
